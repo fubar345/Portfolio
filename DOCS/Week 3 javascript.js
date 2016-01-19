8cf41fc2b26a359e01054832684473686ce7a63e
Tyler <tylerdunbar77@gmail.com>
2016-01-19 15:06:02 -0500

    Added Week 3 javascript file to show examples of code for each day

---
+++ DOCS/Week 3 javascript.js
@@ -0,0 +1,28 @@
+// Tuesday's work
+var myAnswer = "yes"
+if(myAnswer.length === 3) { console.log("I finished my first course!")}
+   
+ else {
+    console.log("I suck!")
+}
+var myColor = "Green"
+console.log(myColor.length);
+
+
+var myName = "Billy";
+
+console.log(myName);
+
+ myName = "Bi";
+ 
+console.log(myName);
+
+var myCountry = "Usa"
+
+console.log( myCountry.length);
+
+console.log( myCountry.substring(0,3));
+
+var myAge = 45;
+
+console.log(myAge);
