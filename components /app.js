1e8fcdee91123aa717b3fb4b8402cf4f7d2894dd
Tyler <tylerdunbar77@gmail.com>
2016-01-19 16:27:46 -0500

    Modified fish animation to better fit screen.

--- components /app.js
+++ components /app.js
@@ -12,14 +12,14 @@ $(document).ready(function(){
 
 
 function gofish(){
-		$('.fish').animate({right: '130%'}, 7000, function(){
+		$('.fish').animate({right: '90%'}, 7000, function(){
 		
 			$(this).css({transform: 'scaleX(-1)'});
 			
 			var height = $(this).height();
-			height += 20;
+			height = (height > 160)? 160: height + 20;
 			$(this).height(height);
-			$(this).animate({right:'-30%'}, 4000, function(){
+			$(this).animate({right:'0'}, 4000, function(){
 				$('.fish').css({transform: 'scaleX(1)'});
 			});
 		
