3c1d988c0739400f176ad61159d4d5d60ec85d53
Tyler <tylerdunbar77@gmail.com>
2016-01-24 22:22:22 -0500

    Added Friday’s work

--- DOCS/Week 3 javascript.js
+++ DOCS/Week 3 javascript.js
@@ -49,3 +49,26 @@ if(age < 13)
       console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
     console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
     var userAnswer = prompt("Do you want to race Beiber on stage?")
+//Friday's work
+ var userAnswer = prompt("Do you want to race Beiber on stage?")
+    if(userAnswer = "yes")
+{console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!")
+    } else{
+   console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'")
+    }
+     var feeback = prompt("Rate my game out of 10!")
+    if(feedback > 8)
+{console.log("Thank you! We should race at the next concert!")
+    } else{
+   console.log("I'll keep practicing coding and racing.")
+    }
+
+
+
+
+
+
+
+
+
+
