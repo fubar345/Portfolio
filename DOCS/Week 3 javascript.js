3a8ac91310ebed7028257d231a257c62e4184e03
Tyler <tylerdunbar77@gmail.com>
2016-01-24 22:14:41 -0500

    Updated javascript file. Forgot to update daily.

--- DOCS/Week 3 javascript.js
+++ DOCS/Week 3 javascript.js
@@ -27,3 +27,25 @@ var myAge = 45;
 
 console.log(myAge);
 //Wednesday's work
+
+confirm("I am ready to play!");
+
+var age = prompt("What's your age?")
+if(age < 13)
+{console.log("You are allow to play but take no responsibility.")
+    } else{
+   console.log("Go ahead and play!")
+    }
+//Thursday's work
+var age = prompt("What's your age?")
+
+if(age < 13)
+{console.log("You are allow to play but take no responsibility.")
+    } else{
+   console.log("Go ahead and play!")
+    }
+    console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
+    
+      console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
+    console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'")
+    var userAnswer = prompt("Do you want to race Beiber on stage?")
