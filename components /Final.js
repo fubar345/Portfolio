cfe87517282f69e349b87fea5ec36f1e42894d7d
Tyler <tylerdunbar77@gmail.com>
2016-05-24 20:41:39 -0400

    Added started javascript file for final project

---
+++ components /Final.js
@@ -0,0 +1,7 @@
+var useranswer = prompt("Hello! Would you like to play?")
+if(useranswer === "yes"){
+	console.log("Ok! Have fun and good luck!")
+}else if(useranswer === "no"){
+	console.log("Aww thats too bad. Please come back again when you want to play.")
+
+};
