a1fb9ff5e18ce951223b3bb86fe2958995780e0b
Tyler <tylerdunbar77@gmail.com>
2016-05-25 00:08:04 -0400

    Added the first choice and fixed code. Still trying to fix undefined error

--- components /Final.js
+++ components /Final.js
@@ -7,7 +7,8 @@ if(useranswer === "yes"){
   console.log("Aww thats too bad.");
   return;
 } else if( useranswer !== "yes" || useranswer !== "no"){
-  console.log("Sorry didn't understand that.")
+  console.log("Sorry didn't understand that.");
+  
 };
 
 })();
@@ -19,12 +20,26 @@ if(userAnswer2 === "yes"){
   console.log("Your character's name is " + name + ".");
 } else if(userAnswer2 === "no"){
 	console.log("Ok then a name will be given.");
-    CharacterName();
-    console.log("Your character's name is " + CharacterName() + ".")
-
+	
 } else if(userAnswer2 !== "yes" || useranswer !== "no" ){
      console.log("Sorry didnt understand that.");
+     
 };
+
+
+//User's first choice
+var useranswer3 = prompt( name + " walks down a heavily forested path. They reach a fork in the path. Will they go right or left?").toLowerCase();
+if(useranswer3 === "right"){
+	console.log( name + " has reached the end of the right pathway. A massive mountain stands in their way and they cannot return the way they came. Your character has reached the end of their journey.");
+	console.log("GAME OVER!");
+} else if( useranswer3 === "left"){
+	console.log(name + " wanders down the left path. They reach a clearing in the dense forest.");
+}else if (useranswer3 !== "right" || useranswer3 !== "left"){
+	console.log("Sorry didn't understand that.");
+	
+};
+
+
 
 // random name generation
 function CharacterName(name){
